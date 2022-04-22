5e05e0dbba1c5569aa6268bc214e2c707019c65e
burigori <jamasa97@icloud.com>
2022-04-22 16:35:29 +0900

    Add : Jpa.ddl 테스트 및 LambdaTest

--- src/main/java/com/example/myspringbootapp/repository/AccountRepository.java
+++ src/main/java/com/example/myspringbootapp/repository/AccountRepository.java
@@ -7,4 +7,15 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
     Account findByUsername(String username);
+
+
+
+
+
+    // application에서
+    // spring jpa hibernate ddl-auto 는 4가지
+    // create 기존 테이블을 삭제하고 새로 만는다
+    // create-drop 종료시점에 삭제한다.
+    // update 기존 스키마유지하고 새로운 것만 추가한다. 기존의 데이터와 변경된 부분도 반영한다!
+    // validate 엔티티와 테이블이 정상 매핑되어있는지 검증한다.
 }
--- src/test/java/com/example/myspringbootapp/repository/AccountRepositoryTest.java
+++ src/test/java/com/example/myspringbootapp/repository/AccountRepositoryTest.java
@@ -1,23 +1,30 @@
 package com.example.myspringbootapp.repository;
 
 import com.example.myspringbootapp.entity.Account;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.sql.DataSource;
+import java.util.Optional;
 
+@Slf4j
 @SpringBootTest
 public class AccountRepositoryTest {
+    // SpringBootTest를 하게되면 Spring Bean을 사용할 수 있게 해준다.
+
 
     @Autowired
     AccountRepository accountRepository;
 
     @Test
+    @Disabled
     public void crud() throws Exception{
         // Insert
         Account account = new Account();
-        account.setUsername("user1");
+        account.setUsername("user");
         account.setPassword("1234");
         accountRepository.save(account);
         //==========
@@ -25,5 +32,15 @@ public class AccountRepositoryTest {
         Account savedAccount = accountRepository.findByUsername("user");
         System.out.println("ID "+savedAccount.getId());
         System.out.println("username "+savedAccount.getUsername());
+    }
+
+    @Test
+    public void finder() throws Exception{
+        Optional<Account> result = accountRepository.findById(1L);
+        Account account =result.get();//없는 값이라면 에러가 발생한다.
+        log.info("Account {}",account);
+
+        Account acct = accountRepository.findById(11L).orElse(new Account());
+        log.info("new Account hear => {}",acct);
     }
 }
