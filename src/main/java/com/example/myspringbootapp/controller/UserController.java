31061ab55ce74f99ad39252b27f16887e1a6d985
burigori <jamasa97@icloud.com>
2022-04-27 11:44:22 +0900

    Feat: Thymeleaf로 html 구현

---
+++ src/main/java/com/example/myspringbootapp/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.example.myspringbootapp.controller;
+
+import com.example.myspringbootapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/index")
+    public String index(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        return "index";
+    }
+}
